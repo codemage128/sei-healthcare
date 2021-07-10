cfec9454425881f1e60c47de25b7b7807d307c64
James <james941001@gmail.com>
2021-07-10 19:47:22 +0000

    change the shipping cart functionality

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,90 +1,108 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
 // Start Humber Main Menu Icon Js
-$('.all-p-humber').click(function(){
-	$(this).toggleClass('open');
-});
+    $('.all-p-humber').click(function () {
+        $(this).toggleClass('open');
+    });
 //==== End Humber Main Menu Icon Js
 
 //==== Start Mobile menu js 
-$('.mobile-menu-icon').click(function(){
-  $('.mobile_main_menu_content ul').slideToggle();
-});
+    $('.mobile-menu-icon').click(function () {
+        $('.mobile_main_menu_content ul').slideToggle();
+    });
 //==== End Mobile menu js 
-$('.close-menu').click(function(){
-    $('.mobile_main_menu_content ul').slideToggle();
-    $('.all-p-humber').toggleClass('open');
-});
+    $('.close-menu').click(function () {
+        $('.mobile_main_menu_content ul').slideToggle();
+        $('.all-p-humber').toggleClass('open');
+    });
 
-$('.close-cart').click(function(){
-    $('.open-cart-menu').slideToggle();
-    // $('.mobile_main_menu_content ul').slideToggle();
-});
-$('.add-cart').click(function(){
-    $('.open-cart-menu').slideToggle();
-})
+    $('.close-cart').click(function () {
+        $('.open-cart-menu').slideToggle();
+        // $('.mobile_main_menu_content ul').slideToggle();
+    });
+    $('.add-cart').click(function () {
+        $('.open-cart-menu').slideToggle();
+
+        var product_name = $(this).data('name');
+        if (product_name == "gle"){
+            $('.gle').show();
+        }
+        if (product_name == "oil"){
+            $('.oil').show();
+        }
+        if (product_name == "sit"){
+            $('.sit').show();
+        }
+    })
+    $('.decrease').click(function () {
+        var id = "#" + $(this).data('id');
+        var current_value = $(id).val();
+        $(id).val(current_value-1);
+    })
+    $('.increase').click(function () {
+        var id = "#" + $(this).data('id');
+        var current_value = $(id).val();
+        var new_value = parseInt(current_value) + 1;
+        $(id).val(new_value);
+    })
 // nase_main_area  carousel area
-$('.toffe_content_area').owlCarousel({
-  items:2,
-  loop:true,
-  nav:false,
-  dots:true,
-  autoplay:true,
-  margin: 40,
-  responsive: {
-    0:{
-        items: 1,
+    $('.toffe_content_area').owlCarousel({
+        items: 2,
+        loop: true,
+        nav: false,
         dots: true,
-    },
-    600:{
-        items: 2,
-    },
-    1000:{
-        items: 2,
-    },
-}
-});
+        autoplay: true,
+        margin: 40,
+        responsive: {
+            0: {
+                items: 1,
+                dots: true,
+            },
+            600: {
+                items: 2,
+            },
+            1000: {
+                items: 2,
+            },
+        }
+    });
 
 // nase_main_area  carousel area
-$('.shop_header_left_slider').owlCarousel({
-    items:1,
-    loop:true,
-    nav:false,
-    dots:true,
-    autoplay:false,
-});
+    $('.shop_header_left_slider').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: false,
+        dots: true,
+        autoplay: false,
+    });
 
 
 // Show Details Js
 
-$('.sinlge-product-kaufen button.one').click(function(){
-    // $('.more-product-details.one').slideToggle();
-});
+    $('.sinlge-product-kaufen button.one').click(function () {
+        // $('.more-product-details.one').slideToggle();
+    });
 
-$('.sinlge-product-kaufen button.two').click(function(){
-    // $('.more-product-details.two').slideToggle();
-});
+    $('.sinlge-product-kaufen button.two').click(function () {
+        // $('.more-product-details.two').slideToggle();
+    });
 
-$('.sinlge-product-kaufen button.three').click(function(){
-    // $('.more-product-details.three').slideToggle();
-});
-
-
+    $('.sinlge-product-kaufen button.three').click(function () {
+        // $('.more-product-details.three').slideToggle();
+    });
 
 
 });
 
 
+$(window).scroll(function () {
+    var scrollValue = $(this).scrollTop();
 
-
-$(window).scroll(function(){
-  var scrollValue = $(this).scrollTop();
-
-  if(scrollValue>200){
-    $('.header_main_area').addClass('fixedHeader');
-}else{
-    $('.header_main_area').removeClass('fixedHeader');
-}
+    if (scrollValue > 200) {
+        $('.header_main_area').addClass('fixedHeader');
+    } else {
+        $('.header_main_area').removeClass('fixedHeader');
+    }
 
 
 });
